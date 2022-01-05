d681c09c40437fd87e47a2406e197d48d02eebe6
muhammedshebinva <muhammedshebinva@gmail.com>
2022-01-05 19:30:35 +0530

    component for row poster added

--- netflix_main/src/App.js
+++ netflix_main/src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import Navbar from "./Components/NavBar/NavBar";
 import './App.css'
 import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Banner/>
+      <RowPost/>
     </div>
   );
 }
