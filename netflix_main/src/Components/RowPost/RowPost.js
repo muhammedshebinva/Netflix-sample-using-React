20328f643133cf0e4cc8994cbd72837cb1fc30ee
muhammedshebinva <muhammedshebinva@gmail.com>
2022-01-06 18:10:11 +0530

    intalled npm react-youtube and added videos of the movie by using there id from the object provideed by the TMDB site

--- netflix_main/src/Components/RowPost/RowPost.js
+++ netflix_main/src/Components/RowPost/RowPost.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import Youtube from 'react-youtube'
 import './RowPost.css'
-import {imageUrl} from '../../Constants/constants'
+import {imageUrl,API_KEY} from '../../Constants/constants'
 import axios from '../../axios'
 function RowPost(props) {
     const [movies, setMovies] = useState([])
+    const [urlId, setUrlId] = useState('')
     useEffect(() => {
         axios.get(props.url).then((response)=>{
             console.log(response);
@@ -12,6 +14,25 @@ function RowPost(props) {
             // alert('Network Error')
         })
      }, [])
+     const opts = {
+        height: '300',
+        width: '100%',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+        },
+      };
+
+      const handleMovie =(id)=>{
+        console.log(id);
+        axios.get(`/movie/${id}/videos?api_key=${API_KEY}`).then(response=>{
+            if(response.data.results.length!==0){
+                setUrlId(response.data.results[0])
+            }else{
+                console.log('Array empty');
+            }
+        })
+      }
     return (
         
         <div className='row'>
@@ -19,12 +40,14 @@ function RowPost(props) {
             <div className="posters">
                 {
                     movies.map((obj)=>
-                        <img className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
+                        <img onClick={()=>handleMovie(obj.id)} className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
 
                     )
                 }
                
             </div>
+            { urlId && <Youtube opts={opts} videoId={urlId.key} />}
+
         </div>
     )
 }
