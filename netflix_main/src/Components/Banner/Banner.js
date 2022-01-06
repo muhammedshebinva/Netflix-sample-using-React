776d9a9b70860234321e6a77b6be920f357d52e9
muhammedshebinva <muhammedshebinva@gmail.com>
2022-01-06 16:49:52 +0530

    axios installed & added banner and Row Poster contents From the urls

--- netflix_main/src/Components/Banner/Banner.js
+++ netflix_main/src/Components/Banner/Banner.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import {API_KEY,imageUrl} from '../../Constants/constants'
 import './Banner.css'
+import axios from '../../axios'
+
 function Banner() {
+
+    const [movie, setMovie] = useState([])
+    useEffect(() => {
+       axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+           console.log(response.data.results[0]);
+           setMovie(response.data.results[0])
+       })
+    }, [])
+
     return (
-        <div className='banner'>
+
+        
+        <div style={{backgroundImage:`url(${ movie ? imageUrl+ movie.backdrop_path : ""})`}} className='banner'>
             <div className='content'>
-                <h1 className='title'>Movie name</h1>
+                <h1 className='title'>{movie ? movie.title : ""}</h1>
                 <div className='banner_buttons'>
                     <button className='button'>Play</button>
                     <button className='button'>My List</button>
                 </div>
-                <h1 className='description'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a documen</h1>
+                <h1 className='description'>{movie ? movie.overview : ""}</h1>
             </div>
             <div className="fade_bottom"></div>
         </div>
